47271dafdf37dd26588ef2e9b811147354497399
Maciej Zapart <maciej.zapart.96@gmail.com>
2022-04-25 22:02:26 +0200

    initial commit

---
+++ baw-backend/src/main/java/com/example/bawbackend/BawBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.bawbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BawBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BawBackendApplication.class, args);
+	}
+
+}
---
+++ baw-backend/src/test/java/com/example/bawbackend/BawBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bawbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BawBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
